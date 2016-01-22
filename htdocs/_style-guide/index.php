05218a51a571630c01094240ce915e8d7b92efa8
Chris May <chris@punchrva.com>
2016-01-22 11:40:25 -0500

    Adding grid options into style guide

--- htdocs/_style-guide/index.php
+++ htdocs/_style-guide/index.php
@@ -20,166 +20,255 @@
 
 <body>
 
+  <div class="content">
+    
+    <h1>Style guide</h1>
 
-  <h1>Style guide</h1>
+    <section>
+      <h2 id="Grid">Grid</h2>
+      <h3 id="Standard-Grid">Standard Grid</h3>
+      <div class="grid">
+        <div class="grid__item" data-grid="1/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>1</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="11/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>11</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="2/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>2</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="10/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>10</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="3/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>3</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="9/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>9</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="4/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>4</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="8/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>8</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="5/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>5</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="7/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>7</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="6/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>6</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="6/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>6</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="7/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>7</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="5/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>5</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="8/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>8</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="4/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>4</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="9/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>9</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="3/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>3</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="10/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>10</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="2/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>2</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="11/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>11</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+        <div class="grid__item" data-grid="1/12">
+          <div class="demo__item">
+            <div class="demo__fraction">
+              <sup>1</sup>⁄<sub>12</sub>
+            </div>
+          </div>
+        </div>
+      </div>
 
-  <h2>Grid</h2>
-  <div class="grid">
-    <div class="grid__item" data-grid="1/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>1</sup>⁄<sub>12</sub>
-        </div>
+      <h3 id="Grid-Modifiers">Grid Modifiers</h3>
+      <h4>Grid Options</h4>
+      <ul>
+        <li>Left</li>
+        <li>Center</li>
+        <li>Right</li>
+        <li>Top</li>
+        <li>Middle</li>
+        <li>Bottom</li>
+        <li>Forwards</li>
+        <li>Backwards</li>
+        <li>Between</li>
+        <li>Around</li>
+        <li>Equal</li>
+      </ul>
+      <h4 id="centered-grid">Centered Grid</h4>
+      <div class="grid" data-grid="center">
+        <div class="grid__item" data-grid="3/12">
+          <div class="demo__item">
+             Centered grid item 
+          </div>
+         </div>
+        <div class="grid__item" data-grid="3/12">
+          <div class="demo__item">
+             Centered grid item 
+          </div>
+         </div>
+        <div class="grid__item" data-grid="3/12">
+          <div class="demo__item">
+             Centered grid item 
+          </div>
+         </div>
       </div>
-    </div>
-    <div class="grid__item" data-grid="11/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>11</sup>⁄<sub>12</sub>
-        </div>
+
+      <h4 id="right-aligned-grid">Right-aligned Grid</h4>
+      <div class="grid" data-grid="right">
+        <div class="grid__item" data-grid="3/12">
+          <div class="demo__item">
+            Grid item 
+          </div>
+         </div>
+        <div class="grid__item" data-grid="3/12">
+          <div class="demo__item">
+            Grid item 
+          </div>
+         </div>
+        <div class="grid__item" data-grid="3/12">
+          <div class="demo__item">
+            Grid item 
+          </div>
+         </div>
       </div>
-    </div>
-    <div class="grid__item" data-grid="2/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>2</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="10/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>10</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="3/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>3</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="9/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>9</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="4/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>4</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="8/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>8</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="5/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>5</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="7/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>7</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="6/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>6</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="6/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>6</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="7/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>7</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="5/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>5</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="8/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>8</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="4/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>4</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="9/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>9</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="3/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>3</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="10/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>10</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="2/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>2</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="11/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>11</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
-    <div class="grid__item" data-grid="1/12">
-      <div class="demo__item">
-        <div class="demo__fraction">
-          <sup>1</sup>⁄<sub>12</sub>
-        </div>
-      </div>
-    </div>
+
+      <h4 id="gridBreakpoint">Grid breakpoint support</h4>
+      <pre>&lt;div class="grid" data-grid="center" data-grid--medium="right" data-grid--large="left"&gt;
+      <!-- 
+      -->&lt;div class="grid__item" data-grid="3/12"&gt; … &lt;/div&gt;
+      <!-- 
+      -->&lt;div class="grid__item" data-grid="3/12"&gt; … &lt;/div&gt;
+      <!-- 
+      -->&lt;div class="grid__item" data-grid="3/12"&gt; … &lt;/div&gt;<!-- 
+      
+      -->&lt;/div&gt;</pre>
+      
+      <h4 id="gridItemMod">Grid item modifiers</h4>
+      <h5>Options</h5>
+      <ul>
+        <li>first</li>
+        <li>last</li>
+        <li>reset-order</li>
+        <li>push</li>
+        <li>pull</li>
+        <li>auto</li>
+      </ul>
+      <pre>&lt;div class="grid__item" data-grid="4/12 last"&gt; 2 &lt;/div&gt;</pre>
+
+      <h4 id="gridGutters">Grid gutters</h4>
+      <p>Create you own gutters in 1grid-settings and use them like grid modifiers at your defined breakpoints. In addition, you can also make use of different vertical gutters – simply prefix them with v--. </p>
+      <pre>&lt;div class="grid" data-grid="none" data-grid--large="wide v--narrow"&gt;</pre>
+
+
+    </section>
+
   </div>
+
 
   
 
