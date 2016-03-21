bd44a98a89d5dc1349925b59c3d521dc5a3da2ca
Chris May <chris@punchrva.com>
2016-03-21 13:26:02 -0400

    Updating gulpfiles and stylesheets with new style directories from #15

--- htdocs/assets/gulpfile.js
+++ htdocs/assets/gulpfile.js
@@ -26,10 +26,10 @@ function swallowError (error) {
 
 // do CSS stuff
 gulp.task('styles', function() {
-  return gulp.src('scss/**/*.scss')
-    //.pipe(sassLint())
-    //.pipe(sassLint.format())
-    //.pipe(sassLint.failOnError())
+  return gulp.src('sass/**/*.scss')
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
     .pipe(sass({ style: 'expanded' }))
     .on('error', swallowError)
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
@@ -71,7 +71,7 @@ gulp.task('clean', function(cb) {
 gulp.task('watch', function() {
 
   // Watch .scss files
-  gulp.watch('scss/**/*.scss', ['styles']);
+  gulp.watch('sass/**/*.scss', ['styles']);
 
   // Watch .js files
   gulp.watch('scripts/**/*.js', ['scripts', 'scripts_copy']);
