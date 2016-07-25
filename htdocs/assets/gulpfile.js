1a9dcaa4edcecf698042368b67f3ea99af229628
Chris May <chris@punchrva.com>
2016-07-25 15:30:10 -0400

    Updating the way autoprefixer expects browser config

--- htdocs/assets/gulpfile.js
+++ htdocs/assets/gulpfile.js
@@ -32,7 +32,9 @@ gulp.task('styles', function() {
     .pipe(scsslint({config: 'scsslint.yml'}))
     .pipe(sass({ style: 'expanded' }))
     .on('error', swallowError)
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(autoprefixer({
+      browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios > 5', 'android 4'],
+    }))
     .pipe(gulp.dest('css'))
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
